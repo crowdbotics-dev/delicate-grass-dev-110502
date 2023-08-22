1454008d69e08a92825d5ee4017ab31ebd370a0c
Crowdbotics <engineering@crowdbotics.com>
2023-08-22 14:39:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-grass-dev-110502",
+  url: "https://delicate-grass-dev-110502.botics.co",
+  api: "https://delicate-grass-dev-110502.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
